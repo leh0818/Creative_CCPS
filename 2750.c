2c1772ee0c92c36e7422da6e5f66a66716648791
leh0818 <65188639+leh0818@users.noreply.github.com>
2020-06-06 23:00:06 +0900

    실험

---
+++ 2750.c
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespaces std;
+
+int main()
+{
+	int n;
+	cin << n;
+	vector <int> v;
+	
+	int t
+	for(int i=0;i<n;i++)
+	{
+		cin << t;
+		v.push_back(t);
+	}
+	
+	sort(v.begin(),v.end());
+	
+	for(int i=0;i<n;i++)
+		cout >> v[i];
+		
+	return 0;
+}
